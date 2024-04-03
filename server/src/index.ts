2eb195e24fe9427aaa75ce1a567459dab65e81cf
ppPacific <90137395+ppPacific@users.noreply.github.com>
2024-04-03 14:26:49 +0800

    Add files via upload

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import {ShiftRouter} from "./routes/shift";
 
-const port = 3001;//process.env.PORT ||
+const port = 3001;
 
 const app = express();
 app.use(express.json());
@@ -11,7 +11,5 @@ app.use(cors());
 
 app.use(ShiftRouter);
 
-//better put in env file
-//mongoose.connect("mongodb+srv://eatdungmon:ecommpw@ecomm.cxodl2x.mongodb.net/ecomm?retryWrites=true&w=majority&appName=ecomm")
 
 app.listen(port, ()=> console.log(`server started on ${port}`))
